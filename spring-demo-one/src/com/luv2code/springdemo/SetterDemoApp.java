fba9cbdf74a1177102edd91e87b5ac983cdaba59
itsLukaszK <lukasz.patryk.krawczyk@gmail.com>
2018-09-13 18:40:19 +0200

    37

---
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -0,0 +1,22 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		// Load the Spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// Retrieve bean from Spring container
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+		// Call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+
+		// Close the context
+		context.close();
+	}
+
+}
